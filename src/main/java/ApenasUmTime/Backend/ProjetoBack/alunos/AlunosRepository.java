cdb1832556d05c8c743b7f529c6b676937669db3
iran <iranilsonhonorato88@gmail.com>
2025-10-30 17:29:20 -0300

    feat: Implementando AlunosRepository

--- src/main/java/ApenasUmTime/Backend/ProjetoBack/alunos/AlunosRepository.java
+++ src/main/java/ApenasUmTime/Backend/ProjetoBack/alunos/AlunosRepository.java
@@ -1,4 +1,10 @@
 package ApenasUmTime.Backend.ProjetoBack.alunos;
 
-public class AlunosRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunosRepository extends JpaRepository<Alunos,Integer> {
+    public Alunos findByCpf(String cpf);
 }
