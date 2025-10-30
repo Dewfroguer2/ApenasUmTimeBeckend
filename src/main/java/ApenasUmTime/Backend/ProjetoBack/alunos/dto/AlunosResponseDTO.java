027d15a79a3914785833c7c1cb9a40710ba01345
iran <iranilsonhonorato88@gmail.com>
2025-10-30 18:46:52 -0300

    feat: Implementando DTOs para Alunos

--- src/main/java/ApenasUmTime/Backend/ProjetoBack/alunos/dto/AlunosResponseDTO.java
+++ src/main/java/ApenasUmTime/Backend/ProjetoBack/alunos/dto/AlunosResponseDTO.java
@@ -1,4 +1,20 @@
 package ApenasUmTime.Backend.ProjetoBack.alunos.dto;
 
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "AlunoReponseDTO", description = "DTO para response da classe Alunos")
 public class AlunosResponseDTO {
+    @Schema(description="Nome do aluno", example="Mordecai")
+    String nome;
+
+    @Schema(description="Email do aluno", example="cabelinho@hotmail.com")
+    String email;
+
+    @Schema(description="Semestre do aluno", example="3")
+    String semestre;
+
+    @Schema(description="Celular do aluno", example="cabelinho@hotmail.com")
+    String celular;
+
 }
