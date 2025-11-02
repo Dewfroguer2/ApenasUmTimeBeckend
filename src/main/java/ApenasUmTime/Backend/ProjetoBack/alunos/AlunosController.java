cfa6472e4168ece9f1146d2bf86a271db4354bd4
iran <iranilsonhonorato88@gmail.com>
2025-11-01 23:46:23 -0300

    feat: Implementando autenticação nos métodos put e post de alunos

--- src/main/java/ApenasUmTime/Backend/ProjetoBack/alunos/AlunosController.java
+++ src/main/java/ApenasUmTime/Backend/ProjetoBack/alunos/AlunosController.java
@@ -3,6 +3,8 @@ package ApenasUmTime.Backend.ProjetoBack.alunos;
 
 import ApenasUmTime.Backend.ProjetoBack.alunos.dto.AlunosRequestDTO;
 import ApenasUmTime.Backend.ProjetoBack.alunos.dto.AlunosResponseDTO;
+import ApenasUmTime.Backend.ProjetoBack.autenticacao.Usuario;
+import ApenasUmTime.Backend.ProjetoBack.autenticacao.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/alunos")
 public class AlunosController {
+
+    @Autowired
+    private UsuarioService usuarioService;
 
     @Autowired
     private AlunosService alunosService;
@@ -34,12 +39,19 @@ public class AlunosController {
     public ResponseEntity<?> cadastrarAlunosController(
             @RequestBody AlunosRequestDTO alunoDTO,
             @RequestHeader("Authorization") String token){
+
+        Usuario usuario = usuarioService.validarToken(token);
         AlunosResponseDTO cadastrado = alunosService.cadastrarAlunos(alunoDTO);
         return ResponseEntity.ok(cadastrado);
     }
 
     @PutMapping("/{cpf")
-    public ResponseEntity<?> editarAlunosController(@PathVariable String cpf, @RequestBody AlunosRequestDTO alunoDTO){
+    public ResponseEntity<?> editarAlunosController(
+            @PathVariable String cpf,
+            @RequestBody AlunosRequestDTO alunoDTO,
+            @RequestHeader("Authorization") String token){
+
+        Usuario usuario = usuarioService.validarToken(token);
         try {
             AlunosResponseDTO alunoEditado = alunosService.editarAlunos(alunoDTO);
             return ResponseEntity.ok(alunoEditado);
