830bd018dccc55c33e56eb95554258bb8e34d89c
iran <iranilsonhonorato88@gmail.com>
2025-11-01 22:56:31 -0300

    feat: implementando autenticação de usuário

---
+++ src/main/java/ApenasUmTime/Backend/ProjetoBack/autenticacao/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package ApenasUmTime.Backend.ProjetoBack.autenticacao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    public Usuario findByEmail(String email);
+}
